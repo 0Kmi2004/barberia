720129779b9425ec91922d95f7734c7d1fdcaca5
0Kmi2004 <marquezcamilo1134@gmail.com>
2026-08-12 19:47:38 -0300

    modal2

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ const modal = document.getElementById("modalReserva");
 
 const abrir = document.getElementById("btnReservar");
 
+const abrir2 = document.getElementById("btnReservar2");
+
 const cerrar = document.getElementById("btnCerrar");
 
 abrir.addEventListener("click", () => {
     modal.classList.add("active");
 });
+
+abrir2.addEventListener("click", () => {
+    modal.classList.add("active");
+});
+
 
 cerrar.addEventListener("click", () => {
     modal.classList.remove("active");
